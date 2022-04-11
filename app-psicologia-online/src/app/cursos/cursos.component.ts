898febc0965d45f8bd49abc0a6faa968652f7870
Fabíola Augusto <fabiolaccaugusto@gmail.com>
2022-04-11 13:14:55 -0300

    cursos

--- app-psicologia-online/src/app/cursos/cursos.component.ts
+++ app-psicologia-online/src/app/cursos/cursos.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Curso } from 'src/app/models/curso.model';
+import { CursoService } from 'src/app/services/curso.service';
+
 @Component({
   selector: 'app-cursos',
   templateUrl: './cursos.component.html',
   styleUrls: ['./cursos.component.css']
 })
 export class CursosComponent implements OnInit {
+  public cursos: Array<Curso> = [ ];
 
-  constructor() { }
+  constructor(private cursoServ: CursoService) { }
 
   ngOnInit(): void {
+    this.cursos = this.cursoServ.getAll();
   }
 
 }
