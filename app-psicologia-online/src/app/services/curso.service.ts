96dae44b7fbb529f1ef9c14b6a92d51e1bcef041
Fabíola Augusto <fabiolaccaugusto@gmail.com>
2022-04-12 10:09:33 -0300

    mudança imagens e componentes

--- app-psicologia-online/src/app/services/curso.service.ts
+++ app-psicologia-online/src/app/services/curso.service.ts
@@ -10,31 +10,31 @@ export class CursoService {
   public cursos: Array<Curso> = [
     {
       id: 0,
-      nome: 'Desenvolvimento Web com Angular 13',
-      descricao: 'Curso muito bom, muito divertido, muito legal',
-      preco: 22.90,
-      foto: './../../assets/img/curso-foto-teste.jpg'
+      nome: 'As 7 Leis Herméticas',
+      descricao: 'Curso sobre as sete principais leis herméticas se baseiam nos princípios incluídos no livro Caibalion',
+      preco: 1000.00,
+      foto: './../../assets/img/curso-leishermeticas.jpg'
     },
     {
       id: 1,
-      nome: 'Criação de APIs com Node.js',
-      descricao: 'Curso muito bom, muito divertido, muito legal',
-      preco: 22.90,
-      foto: './../../assets/img/curso-foto-teste.jpg'
+      nome: 'Ressonância Harmônica',
+      descricao: 'O Curso de Ressonância Harmônica tiliza ondas de informação que limpam gradativamente crenças limitantes e inserem no indivíduo novas informações para alavancar seu crescimento, em todas as áreas. ',
+      preco: 1050.00,
+      foto: './../../assets/img/curso-ressonancia.jpg'
     },
     {
       id: 2,
-      nome: 'Python de A a Z',
-      descricao: 'Curso muito bom, muito divertido, muito legal',
-      preco: 22.90,
-      foto: './../../assets/img/curso-foto-teste.jpg'
+      nome: 'Expansão de Consciência',
+      descricao: 'O Curso de expansão de consciência envolve a ampliação das percepções sobre si mesmo e em relação ao mundo ao seu redor.',
+      preco: 1100.00,
+      foto: './../../assets/img/images.jpeg'
     },
     {
       id: 3,
-      nome: 'Spring Boot 2.2',
-      descricao: 'Curso muito bom, muito divertido, muito legal',
-      preco: 22.90,
-      foto: './../../assets/img/curso-foto-teste.jpg'
+      nome: 'Biografia de Neville Goddard',
+      descricao: 'Neville Goddard dedicou-se a ilustrar os ensinamentos da verdade psicológica escondida nos ensinamentos bíblicos, e despertar a consciência do verdadeiro significado que os antigos pretenderam trazer ao mundo, mas que precisaram ocultar a verdadeira mensagem apenas àqueles que alcançaram o entendimento, durante uma época de tantas guerras e perseguições, em meio à antiguidade.',
+      preco: 1200.00,
+      foto: './../../assets/img/curso-neville.jpg'
     }
   ];
 
